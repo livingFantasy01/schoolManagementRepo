03a08e0ea560ff173e2cb7e3b8e3ff96e396069a
rahul payal <payalrahul45@gmail.com>
2020-07-04 20:55:13 +0530

    student service

---
+++ StudentService/src/main/java/com/student/beans/Student.java
@@ -0,0 +1,109 @@
+package com.student.beans;
+
+import java.util.Date;
+
+public class Student {
+	
+	private String id;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String fathersName;
+	private String mothersName;
+	private Date dob;
+	private Date dateOfJoining;
+	private Address temporaryAddress;
+	private Address permanentAddress;
+	private String emailId;
+	private String phoneNumber;
+	private String alternateNumber;
+	private Boolean isActive;
+							
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFathersName() {
+		return fathersName;
+	}
+	public void setFathersName(String fathersName) {
+		this.fathersName = fathersName;
+	}
+	public String getMothersName() {
+		return mothersName;
+	}
+	public void setMothersName(String mothersName) {
+		this.mothersName = mothersName;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public Date getDateOfJoining() {
+		return dateOfJoining;
+	}
+	public void setDateOfJoining(Date dateOfJoining) {
+		this.dateOfJoining = dateOfJoining;
+	}
+	public Address getTemporaryAddress() {
+		return temporaryAddress;
+	}
+	public void setTemporaryAddress(Address temporaryAddress) {
+		this.temporaryAddress = temporaryAddress;
+	}
+	public Address getPermanentAddress() {
+		return permanentAddress;
+	}
+	public void setPermanentAddress(Address permanentAddress) {
+		this.permanentAddress = permanentAddress;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getAlternateNumber() {
+		return alternateNumber;
+	}
+	public void setAlternateNumber(String alternateNumber) {
+		this.alternateNumber = alternateNumber;
+	}
+	public Boolean getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+	
+	
+}
---
+++ StudentService/src/main/java/com/student/constants/CommonConstants.java
@@ -0,0 +1,20 @@
+package com.student.constants;
+
+public class CommonConstants {
+	
+	public static final String STUDENT_SEQUENCE = "studentSequence";
+	public static final String  STUDENT_SEQUENCE_LEN = "000000";
+	public static final String ID = "id";
+	public static final String FIRST_NAME = "firstName";
+	public static final String MIDDLE_NAME = "middleName";
+	public static final String LAST_NAME = "lastName";
+	public static final String FATHER_NAME = "fathersName";
+	public static final String MOTHER_NAME = "mothersName";
+	public static final String DOB = "dob";
+	public static final String DOJ = "dateOfJoining";
+	public static final String EMAIL_ID = "emailId";
+	public static final String PHONE_NUMBER = "phoneNumber";
+	public static final String ALTERNATE_NUMBER = "alternateNumber";
+	public static final String IS_ACTIVE = "isActive";
+
+}
